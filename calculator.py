fc94e13bad64c0e09afe3ddfd6dd605e7698b3f8
saikrishnakalle <kallesaikrishna2@gmail.com>
2026-04-07 22:22:09 +0530

    first code
    Change-Id: I90aed9f088822631b64a15a7754cc206f84c690a

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("Addition:", add(5, 3))
+    print("Subtraction:", sub(5, 3))
+    print("Multiplication:", mul(5, 3))
+    print("Division:", div(5, 3))
